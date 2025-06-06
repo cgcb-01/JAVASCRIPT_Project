2cb515ff5b3d57b009975470892ffb79895bb479
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-06 15:29:11 +0530

    animation

---
+++ Rock,paper,Scissor game/app.js
@@ -0,0 +1,11 @@
+// The concept to be used here is:
+// The gamefield will be displayed and the choose will be hidden initially 
+// Play button on click will make choose field displayed and gamefield display=none
+// on choosing any symbols store the button[i] then make the choose field none and show gamefield.
+// save the i of button chosen by user.
+// List of img src. random value between 1 to 3.
+// choose the index of the list acc to random value .
+// so user image index is i .
+// load game field ->ratate the stone image twice then change the img.src to new src.
+// if i=random value by computer draw.
+//and as the rule is applied apply winner rules.
